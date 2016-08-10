ad80a4924dc8cd87d3c5aacd59d962c3f8c39e12
kadoshita <kadoshita.inct@gmail.com>
2016-08-10 14:42:13 +0900

    add bin mode

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var isClick=false;
-
+var mode=getMode();
 setInterval("count1()", 1000 );
 
 function count1(){
-	var date, Year, countYear, tmp, Msec, countDay, countHour, countMin, countSec,
+	var date, Year, countYear, tmp, Msec, countDay, countHour, countMin, countSec;
+	var br=document.getElementById("newline");
 	date = new Date();
 	Year = date.getFullYear();
 	countYear = new Date( 2016 , 9 , 8 ,00,00,00);
@@ -22,10 +23,34 @@ function count1(){
 	countSec = Math.floor ( Msec / 1000);
 	changeColor1(countDay);
 	myDisp = "";
-	if ( countDay != 0 ) myDisp += '<span class=\"dseg\">'+countDay + "</span>日<span class=\"br\">";
-	if ( countHour != 0 ) myDisp += '<span class=\"dseg\">'+countHour + "</span>時間 ";
-	if ( countMin != 0 ) myDisp += '<span class=\"dseg\">'+countMin + "</span>分 ";
-	myDisp += '<span class=\"dseg\">'+countSec + "</span>秒";
+	if(mode=="bin"){
+		if(countDay != 0){
+			myDisp += '<span class=\"dseg\">'+countDay.toString(2) + "</span>日<span class=\"br\">";
+		}
+		if(countHour != 0){
+			myDisp += '<span class=\"dseg\">'+countHour.toString(2) + "</span>時間<br> ";
+		}
+		if(countMin != 0){
+			myDisp += '<span class=\"dseg\">'+countMin.toString(2) + "</span>分 ";
+		}
+		myDisp += '<span class=\"dseg\">'+countSec.toString(2) + "</span>秒";
+		var tmp=27;
+		document.getElementById("no27").innerHTML=tmp.toString(2);
+		br.innerHTML="<br>"
+	}
+	else{
+		if(countDay != 0){
+			myDisp += '<span class=\"dseg\">'+countDay + "</span>日<span class=\"br\">";
+		}
+		if(countHour != 0){
+			myDisp += '<span class=\"dseg\">'+countHour + "</span>時間 ";
+		}
+		if(countMin != 0){
+			myDisp += '<span class=\"dseg\">'+countMin + "</span>分 ";
+		}
+		myDisp += '<span class=\"dseg\">'+countSec + "</span>秒";
+		document.getElementById("no27").innerHTML="27";
+	}
 	document.getElementById("countdown1").innerHTML = myDisp;
 }
 function changeColor1(count){
@@ -83,4 +108,15 @@ function changeText(){
 		sinchoku.innerHTML="進捗どうですか?";
 		isClick=false;
 	}
+}
+function getMode() {
+	var url = document.location.href;
+	var args = url.split('?');
+	if (args.length > 1) {
+		var mode = args[1];
+		if (mode != ""){
+			return mode;
+		}
+	}
+	return ;
 }

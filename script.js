537eb0bec7e034938cde47524066e9a23b472070
kadoshita <kadoshita.inct@gmail.com>
2016-10-08 23:54:57 +0900

    delete change color

--- script.js
+++ script.js
@@ -24,7 +24,6 @@ counter=setInterval(function(){
 	Msec -= ( countMin * (1000*60) );
 	
 	countSec = Math.floor ( Msec / 1000);
-	changeColor(countDay);
 	myDisp = "";
 	if(mode=="bin"){
 		if(countDay != 0){
@@ -57,51 +56,6 @@ counter=setInterval(function(){
 	document.getElementById("countdown1").innerHTML = myDisp;
 }, 1000 );
 
-function changeColor(count){
-	var count1=document.getElementById("count1");
-	var dialog1=document.getElementById("dialog1");
-	var sinchoku=document.getElementById("sinchoku");
-	if(count>=100){
-		document.body.style.backgroundColor = "#ffffff";
-		count1.style.backgroundColor="#fafafa";
-		count1.style.borderColor="#59b1eb";
-		count1.style.color="#1976d2";
-		sinchoku.style.color="#1976d2";
-		dialog1.src=("./gif_b.gif");
-	}
-	else if(count>=50){
-		document.body.style.backgroundColor = "#ffffff";
-		count1.style.backgroundColor="#fafafa";
-		count1.style.borderColor="#8bc34a";
-		count1.style.color="#689f38";
-		sinchoku.style.color="#689f38";
-		dialog1.src=("./gif_g.gif");
-	}
-	else if(count>=30){
-		document.body.style.backgroundColor = "#fafafa";
-		count1.style.backgroundColor="#eeeeee";
-		count1.style.borderColor="#ffeb3b";
-		count1.style.color="#ffa000";
-		sinchoku.style.color="#ffa000";
-		dialog1.src=("./gif_y.gif");
-	}
-	else if(count>=7){
-		document.body.style.backgroundColor = "#000000";
-		count1.style.backgroundColor="#310D01";
-		count1.style.borderColor="#521F00";
-		count1.style.color="#FF7900";
-		sinchoku.style.color="#FF7900";
-		dialog1.src=("./gif.gif");
-	}
-	else{
-		document.body.style.backgroundColor = "#000000";
-		count1.style.backgroundColor="#000000";
-		count1.style.borderColor="#ff0000";
-		count1.style.color="#ff0000";
-		sinchoku.style.color="#ff0000";
-		dialog1.src=("./gif_r.gif");
-	}
-}
 function changeText(){
 	var sinchoku=document.getElementById('sinchoku');
 	if(!isClick){
